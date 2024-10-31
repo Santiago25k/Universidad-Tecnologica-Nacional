29fd412bcc3271a9ad51aeb1658538c65fc813cc
Aquiles <santtialbiazul25@gmail.com>
2024-10-30 23:26:48 -0300

    add auth, authRoutes

--- plataforma-educativa/app.js
+++ plataforma-educativa/app.js
@@ -3,19 +3,26 @@ const dotenv = require('dotenv');
 const app = express();
 const courseRoutes = require('./routes/courseRoutes');
 const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 //conexion a la base de datos
 const db = require('./config/db');
 
-// Rutas
+
+//middleware para procesar JSON
+app.use(express.json());
+
+//rutas
 app.use('/api/courses', courseRoutes);
 app.use('/api/users', userRoutes);
+
+//rutas de autenticacion
+app.use('/api/auth', authRoutes);
+
 
 //cargar variables de entorno
 dotenv.config();
 
-//middleware para procesar JSON
-app.use(express.json());
 
 //definir puerto e iniciar servidor
 const PORT = process.env.PORT || 5000;
