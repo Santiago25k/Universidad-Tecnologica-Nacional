48d27f675ea0f1d22db7ab762b2fde8b7fb3ee92
Aquiles <santtialbiazul25@gmail.com>
2024-11-02 07:56:23 -0300

    Sin cambios

--- plataforma-educativa/routes/authRoutes.js
+++ plataforma-educativa/routes/authRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
+const authUserController = require('../controllers/authController');
+const { authAdmin } = require("../controllers/authController");
 
 //ruta de registro
-router.post('/register', authController.register);
+router.post('/register', authUserController.register);
 
 //ruta inicio sesion
-router.post('/login', authController.login);
+router.post('/login', authUserController.login);
+
+
 
 module.exports = router;
