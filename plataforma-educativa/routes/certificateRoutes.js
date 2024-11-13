fefbd6e1ae183362c65699797f608282a026f516
Aquiles <santtialbiazul25@gmail.com>
2024-11-13 08:40:15 -0300

    add routes certificateRoutes.js

--- plataforma-educativa/routes/certificateRoutes.js
+++ plataforma-educativa/routes/certificateRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const certificateController = require('../controllers/certificateController');
 
-router.get('/certificado/:id_usuario/:id_curso', certificateController.generateCertificate);
+
+router.get('/usr/:id_usuario/cursos/:id_curso/certificado', certificateController.generateCertificate);
+
 
 module.exports = router;
