bcfa269e7857d953d54b43f0b0b20c407ac409db
Aquiles <santtialbiazul25@gmail.com>
2024-11-25 10:39:23 -0300

    Reorganización de carpetas y archivos

--- plataforma-educativa/routes/certificateRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const certificateController = require('../controllers/certificateController');
-
-
-router.get('/usr/:id_usuario/cursos/:id_curso/certificado', certificateController.generateCertificate);
-
-
-module.exports = router;
--- plataforma-educativa/routes/suscribeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const suscribeController = require('../controllers/suscribeController');
-
-
-router.post('/usr/:id_usuario/suscribir', suscribeController.suscribeUsuarios);
-
-module.exports = router;
