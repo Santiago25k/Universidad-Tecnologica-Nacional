e5f7a1c8e41f9a5fc1ae869fa707655d24e1f775
Aquiles <santtialbiazul25@gmail.com>
2024-11-25 16:46:35 -0300

    Eliminé carpetas y archivos innecesarios

--- plataforma-educativa/client/diseño/landing/js/navbar_responsive.js
+++
@@ -1,6 +0,0 @@
- document.getElementById('hamburger').addEventListener('click', function() {
-      const navLinks = document.getElementById('nav-links');
-      navLinks.classList.toggle('active'); // Alternar la clase active
-  });
-
-  
